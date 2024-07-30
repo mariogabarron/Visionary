ae644e31c85856061ed75a454f27a83938fa5890
Mario <mariogabaron@gmail.com>
2024-07-30 11:40:31 +0200

    eliminar import de editarobjetivo_showdialog.dart

--- lib/utilities/showdialogs/homepage/editarobjetivo_showdialog.dart
+++ lib/utilities/showdialogs/homepage/editarobjetivo_showdialog.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:visionary/utilities/buildinputfield.dart';
 
 void showAlertBottomEditarObjetivo(BuildContext context, String objetivo) {
   showModalBottomSheet(
