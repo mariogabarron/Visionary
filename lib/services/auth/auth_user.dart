70b56d8b252b3edcfc76a1de4dbe6ab1d223937d
splashling <splashling1789@gmail.com>
2024-08-03 21:02:30 +0200

    Añadida la función de registro con email.

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,8 +15,8 @@ class AuthUser {
 */
 
 /// Inicia sesión con email y contraseña, y devuelve los credenciales del usuario y una excepción no simultáneamente nulables.
-/// - Si el inicio de sesión es satisfactorio, devolverá los credenciales del usuario.
-/// - Si ocurre un error, no se devolverán credenciales, y se devolverá una excepción que se podrá controlar.
+/// - Si el inicio de sesión es satisfactorio, devolverá los credenciales del usuario en la primera posición de la tupla.
+/// - Si ocurre un error, no se devolverán credenciales, y se devolverá una excepción [FirebaseAuthException] que se podrá controlar.
 Future<(UserCredential?, FirebaseAuthException?)> login_with_email(String email, String password) async {
   try {
     return (await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password), null);
@@ -25,4 +26,24 @@ Future<(UserCredential?, FirebaseAuthException?)> login_with_email(String email,
   }
 }
 
-
+/// Crea un usuario con email y contraseña, enviando el email de confirmación al correo electrónico dado, y guardando su nombre en la base de datos.
+/// - Si la función termina satisfactoriamente, se realiza un logout para que no se inicie sesión sin estar verificado el usuario. Se devuelve null.
+/// - Si la función tiene un error, se devuelve la [FirebaseAuthException] resultante del error.
+/// - Si la función tiene un error debido a que ya existe un usuario registrado con ese email, antes de lanzar un error, reenvía el correo de confirmación.
+Future<FirebaseAuthException?> register_with_email(String name, String email, String password) async {
+  try {
+    var cred = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+    await cred.user?.sendEmailVerification();
+    await FirebaseDatabase.instance.ref("users").child(cred.user!.uid).set({"name":name});
+    await FirebaseAuth.instance.signOut();
+    return null;
+  }
+  on FirebaseAuthException catch(e) {
+    print(e.code);
+    if(e.code == "email-already-in-use") {
+      var a = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      a.user?.sendEmailVerification();
+    }
+    return e;
+  }
+}
