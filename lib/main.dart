f79a636d29874a27fbaa8a88d00eb043c4438236
splashling <splashling1789@gmail.com>
2024-08-03 13:17:13 +0200

    Conexión al firebase de prueba configurada

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:visionary/routes/routes.dart';
 import 'package:visionary/views/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     title: 'Visionary',
     theme: ThemeData(
