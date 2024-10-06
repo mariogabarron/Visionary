c96d52c9c328bcd6433e5ee2059c3c13e4a3e5fd
Javier Albero <splash.creeper1789@gmail.com>
2024-10-06 18:01:02 +0200

    Reformateados los nombres de las variables en tarea_class.dart

--- lib/services/objects/tarea_class.dart
+++ lib/services/objects/tarea_class.dart
@@ -1,22 +1,33 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:visionary/services/db/db_user_management.dart';
 import 'package:visionary/services/objects/recordatorio.dart';
 
 class Tarea {
-  String name;
-  int priority;
-  int need_done;
-  int times_done;
-  List<DateTime> dates;
-  Recordatorio? recordatorio;
+  String _name;
+  int _priority;
+  int _needDone;
+  int _timesDone;
+  List<DateTime> _dates;
+  Recordatorio? _recordatorio;
+  String _dbRef;
 
-  Tarea({
-        required this.name,
-        required this.priority,
-        required this.need_done,
-        this.dates = const [],
-        this.times_done = 0,
-        this.recordatorio
-      });
+  static final Set<String> _keys = {
+    'name',
+    'priority',
+    'need_done',
+    'times_done',
+    'dates',
+    'reminder'
+  };
+
+  Tarea(String objectiveRef, {
+        required String name,
+        required int priority,
+        required int need_done,
+        required Recordatorio? recordatorio}) : _recordatorio = recordatorio, _needDone = need_done, _priority = priority, _name = name,
+        _dates = const [],
+        _timesDone = 0,
+        _dbRef = userRoute()!.child(objectiveRef).child("tasks").push().path;
 
   static Future<List<Tarea>> from_ref(DatabaseReference ref) async {
     throw UnimplementedError("No implementado");
@@ -25,24 +36,24 @@ class Tarea {
 
 
   bool isDone() {
-    return need_done == times_done;
+    return _needDone == _timesDone;
   }
 
   bool isPeriodic() {
-    return need_done != 1;
+    return _needDone != 1;
   }
 
   void makeDone() {
-    if(times_done < need_done) {
-      times_done++;
-      dates.add(DateTime.now());
+    if(_timesDone < _needDone) {
+      _timesDone++;
+      _dates.add(DateTime.now());
     }
   }
 
   void makeUndone() {
-    if(times_done > 0) {
-      times_done--;
-      dates.removeLast();
+    if(_timesDone > 0) {
+      _timesDone--;
+      _dates.removeLast();
     }
   }
 
