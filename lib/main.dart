6e46cceb4bc9e0198e3502f5848d674f7b6717c0
splashling <splashling1789@gmail.com>
2024-10-07 12:52:30 +0200

    El main ahora es asíncrono

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ import 'package:visionary/views/homepage/homepage_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseDatabase.instance.setPersistenceEnabled(true);
-  MobileAds.instance.initialize();
+  await MobileAds.instance.initialize();
   runApp(MaterialApp(
     title: 'Visionary',
     theme: ThemeData(
