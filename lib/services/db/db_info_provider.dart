3dfd79bfee309a011eb3c3c1e10d95a0ea4d7f37
mario <69764590+mariogabarron@users.noreply.github.com>
2024-10-08 15:28:56 +0200

    llaves en if-else statement

--- lib/services/db/db_info_provider.dart
+++ lib/services/db/db_info_provider.dart
@@ -5,7 +5,9 @@ const String COMPATIBLE_DB_SCHEME = "1.0";
 
 /// Obtiene la versión de esquema de la base de datos.
 Future<String> dbScheme() async {
-  return (await FirebaseDatabase.instance.ref("info").child("db_scheme").get()).value.toString();
+  return (await FirebaseDatabase.instance.ref("info").child("db_scheme").get())
+      .value
+      .toString();
 }
 
 /// Determina si la versión v1 es estrictamente más reciente que la versión v2.
@@ -13,18 +15,24 @@ bool isGreaterVersion(String v1, String v2) {
   var numbers1 = v1.split(".").map((x) => int.parse(x));
   var numbers2 = v2.split(".").map((x) => int.parse(x));
 
-  if(numbers1.elementAt(0) > numbers2.elementAt(0)) return true;
-  else if (numbers1.elementAt(0) < numbers2.elementAt(0)) return false;
-  else {
-    if(numbers1.elementAt(1) > numbers2.elementAt(1)) return true;
-    else return false;
+  if (numbers1.elementAt(0) > numbers2.elementAt(0)) {
+    return true;
+  } else if (numbers1.elementAt(0) < numbers2.elementAt(0)) {
+    return false;
+  } else {
+    if (numbers1.elementAt(1) > numbers2.elementAt(1)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
 /// Determina si el esquema de la base de datos es compatible (válido) para la versión de la aplicación.
 Future<bool> validScheme() async {
-  if(COMPATIBLE_DB_SCHEME == "*") return true;
-  else {
-    return ! isGreaterVersion(await dbScheme(), COMPATIBLE_DB_SCHEME);
+  if (COMPATIBLE_DB_SCHEME == "*") {
+    return true;
+  } else {
+    return !isGreaterVersion(await dbScheme(), COMPATIBLE_DB_SCHEME);
   }
 }
