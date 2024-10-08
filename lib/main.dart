fabb33462020c90a46f540bd0a63aed7cb4ec3fa
splashling <splashling1789@gmail.com>
2024-10-08 09:59:16 +0200

    Ahora el inicio de sesión es perisstente

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:visionary/routes/routes.dart';
-import 'package:visionary/views/homepage/homepage_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:visionary/views/tutorial_inicio/tutorial_uno.dart';
 import 'firebase_options.dart';
@@ -15,6 +15,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
   FirebaseDatabase.instance.setPersistenceEnabled(true);
   await MobileAds.instance.initialize();
   runApp(MaterialApp(
