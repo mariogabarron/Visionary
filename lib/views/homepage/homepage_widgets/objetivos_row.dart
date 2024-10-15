e3a9002070f079bea4203d19c34facf12f441d3e
mario <69764590+mariogabarron@users.noreply.github.com>
2024-10-15 22:50:37 +0200

    Configuración de crear, editar y eliminar objetivo terminada

--- lib/views/homepage/homepage_widgets/objetivos_row.dart
+++ lib/views/homepage/homepage_widgets/objetivos_row.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:visionary/routes/routes.dart';
 import 'package:visionary/services/objects/objetivo_class.dart';
 import 'package:visionary/services/objects/visionary_user_class.dart';
-import 'package:visionary/utilities/showdialogs/homepage/editarobjetivo_showdialog.dart';
 
 class ObjetivosRow extends StatefulWidget {
   const ObjetivosRow({super.key});
@@ -14,11 +13,13 @@ class ObjetivosRow extends StatefulWidget {
 
 class _ObjetivosRowState extends State<ObjetivosRow> {
   late TextEditingController controller;
+  late Future<VisionaryUser> _futureUser;
 
   @override
   void initState() {
     super.initState();
     controller = TextEditingController();
+    reloadData();
   }
 
   @override
@@ -27,13 +28,24 @@ class _ObjetivosRowState extends State<ObjetivosRow> {
     controller.dispose();
   }
 
+  void reloadData() {
+    setState(() {
+      _futureUser = VisionaryUser.fromLogin();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<VisionaryUser>(
-      future: VisionaryUser.fromLogin(),
+      future: _futureUser,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
+          return const Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFFEFCEE)),
+              strokeWidth: 3.0,
+            ),
+          );
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else if (snapshot.hasData) {
@@ -74,7 +86,8 @@ class _ObjetivosRowState extends State<ObjetivosRow> {
                                       await Objetivo.fromRef(objetivo.$2);
                                   if (context.mounted) {
                                     showAlertBottomEditarObjetivo(
-                                        context, obj, controller);
+                                        context, obj, controller, reloadData);
+                                    reloadData();
                                   }
                                 },
                                 child: Text(
@@ -122,4 +135,219 @@ class _ObjetivosRowState extends State<ObjetivosRow> {
       },
     );
   }
+}
+
+void showAlertBottomEditarObjetivo(BuildContext context, Objetivo objetivo,
+    TextEditingController controller, Function reloadData) {
+  String nombreObjetivo = objetivo.name;
+  controller.text = objetivo.name;
+
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    backgroundColor: const Color(0xFFFEFCEE),
+    builder: (BuildContext context) {
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 10),
+                Text(
+                  'Editar objetivo "$nombreObjetivo"',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    color: const Color(0xFF26272C),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  'Cambiar nombre al objetivo $nombreObjetivo',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                    color: const Color.fromARGB(183, 40, 40, 40),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                _buildInputField(
+                    label: "",
+                    inputType: TextInputType.name,
+                    hintText: "Escribe el nuevo nombre",
+                    maxWords: 20,
+                    controller: controller),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[300],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () async {
+                    VisionaryUser u = await VisionaryUser.fromLogin();
+                    objetivo.edit(controller.text, objetivo.motive);
+                    u.updateObjectives();
+                    if (context.mounted) {
+                      Navigator.of(context).pop(); // Cerrar el modal
+                    }
+                  },
+                  child: Text(
+                    'Guardar',
+                    style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                const Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+                  child: Divider(color: Colors.black38),
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  'Eliminar objetivo $nombreObjetivo',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                    color: const Color.fromARGB(183, 40, 40, 40),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[300],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        'Cancelar',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF6D97AC),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () async {
+                        objetivo.deleteObjective();
+                        VisionaryUser u = await VisionaryUser.fromLogin();
+                        u.updateObjectives();
+                        if (context.mounted) {
+                          Navigator.of(context).pop(); // Cerrar el modal
+                        }
+                      },
+                      child: Text(
+                        'Borrar',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 70),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  ).then((_) {
+    reloadData();
+  });
+}
+
+Widget _buildInputField(
+    {required String label,
+    String? hintText,
+    bool obscureText = false,
+    required TextInputType inputType,
+    int? maxWords,
+    required TextEditingController controller}) {
+  TextInputType keyboardType = TextInputType.text;
+
+  if (inputType == TextInputType.emailAddress) {
+    keyboardType = TextInputType.emailAddress;
+  } else if (inputType == TextInputType.visiblePassword) {
+    keyboardType = TextInputType.visiblePassword;
+  }
+
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: GoogleFonts.poppins(
+            color: const Color(0xFFFEFCEE),
+            fontWeight: FontWeight.normal,
+            fontStyle: FontStyle.normal,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 8.0),
+        Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(66, 254, 252, 238),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: TextField(
+            keyboardType: keyboardType,
+            style: GoogleFonts.poppins(
+              color: const Color(0xFFFEFCEE),
+              fontWeight: FontWeight.bold,
+            ),
+            maxLength: maxWords,
+            obscureText: obscureText,
+            controller: controller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromARGB(66, 76, 76, 76),
+              hintText: hintText,
+              hintStyle: GoogleFonts.poppins(
+                color: const Color(0xFFFEFCEE),
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
